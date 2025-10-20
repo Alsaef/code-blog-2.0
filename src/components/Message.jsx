2daf7dea6c0be18a4538fd45d40fdf948f10f744
Alsaef <saef.ratul@gmail.com>
2025-10-20 18:13:22 +0600

    react 2nd  commit

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -8,7 +8,7 @@ const Message = () => {
  <div>
 
    {
-    isPopup&&  <div className="fixed top-[550px] right-7">
+    isPopup&&  <div className="fixed top-[510px] right-7">
   <div className="flex items-start gap-3 bg-white shadow-lg border border-gray-200 rounded-2xl p-4 w-80">
     {/* Bot Icon */}
     <div className="w-10 h-10 bg-blue-100 rounded-full flex items-center justify-center text-blue-600 text-xl font-bold">
@@ -28,14 +28,14 @@ const Message = () => {
         className="input input-bordered border border-black w-full mt-3 text-black bg-white text-sm  focus:ring-2 focus:ring-blue-400"
       />
 
-      <button  className='btn btn-primary mt-4'>Chat</button>
+      <button className='btn btn-primary mt-4'>Chat</button>
     </div>
   </div>
 </div>
 
 
   }
-        <div className="fixed top-[750px] right-11">
+        <div className="fixed top-[700px] right-11">
 
 
 
