e306f25b222771346354bb71e62b15b0030b12f3
Alsaef <saef.ratul@gmail.com>
2025-11-10 13:42:35 +0600

    2nd commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -68,6 +68,10 @@ description:
     index: true,
     follow: true,
   },
+
+   verification: {
+    google: "Qg2pUp6Qa7gEdG4gR04MyuxABxPEx86SxtROXl-P_Po",
+  },
 };
 
 export default function RootLayout({ children }) {
@@ -77,6 +81,7 @@ export default function RootLayout({ children }) {
       cz-shortcut-listen="true"
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        
         <Provider>
           <header>
             <NavBar></NavBar>
